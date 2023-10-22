774deb9e5035e97085c6054c7fb913577b0414cc
Wenkai Li <wenkaili@wenkais-mbp.mynetworksettings.com>
2023-10-22 13:28:19 -0400

    modify roberta finetune

--- src/roberta_finetuning.py
+++ src/roberta_finetuning.py
@@ -1,69 +1,84 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from sklearn.metrics import precision_recall_fscore_support
 from transformers import AutoTokenizer, AutoModelForTokenClassification, Trainer, TrainingArguments, DataCollatorForTokenClassification
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
 import numpy as np
 import torch.nn.functional as F
+from accelerate import Accelerator
 
 # 1. load dataset
-dataset = load_dataset("conll2003")
-label_list = [i for i in range (9)]
+def load_custom_dataset(file_path):
+    sentences = []
+    ner_tags = []
+    tokens = []
+    tags = []
+    all_tags = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            split_line = line.strip().split()
+            if len(split_line) > 1:    
+                tokens.append(split_line[0])
+                tags.append(split_line[-1])
+                all_tags.append(split_line[-1])
+                if split_line[0] in {'.', '!', '?'}:
+                    sentences.append(tokens)
+                    ner_tags.append(tags)
+                    tokens, tags = [], []  # Reset for next sentence
+    return {"tokens": sentences, "ner_tags": ner_tags}, all_tags
+
+
+dataset, all_tags = load_custom_dataset('output.conll')
+# dataset = Dataset.from_dict(dataset)
+label_list = np.unique(all_tags).tolist()
+label_to_id = {label: i for i, label in enumerate(label_list)}
 # 2. load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("roberta-base", add_prefix_space=True)
-model = AutoModelForTokenClassification.from_pretrained("roberta-base", num_labels=9)
+model_directory_path = '/home/zw3/11711-assignment-2/checkpoints_linebyline_1000e'
+tokenizer = AutoTokenizer.from_pretrained(model_directory_path, add_prefix_space=True)
+model = AutoModelForTokenClassification.from_pretrained(model_directory_path, num_labels=len(label_list))
 data_collator = DataCollatorForTokenClassification(tokenizer)
+# Split the dataset into train, validation, and test sets
+def split_dataset(dataset):
+    train_data, test_data = train_test_split(dataset['tokens'], test_size=0.2, random_state=42)
+    train_tags, test_tags = train_test_split(dataset['ner_tags'], test_size=0.2, random_state=42)
+    train_data, valid_data = train_test_split(train_data, test_size=0.1, random_state=42)
+    train_tags, valid_tags = train_test_split(train_tags, test_size=0.1, random_state=42)
+    return train_data, valid_data, test_data, train_tags, valid_tags, test_tags
+
+def tokenize_function(examples):
+    labels = [[label_to_id[label] for label in label_sequence] for label_sequence in examples['ner_tags']]
+    input_ids = tokenizer(examples['tokens'], is_split_into_words=True, padding='max_length', truncation=True)['input_ids']
+    return {
+        "input_ids": input_ids,
+        "labels": labels
+    }
 
 # 3. data preprocessing
-def tokenize_function(examples):
-      return tokenizer(examples['tokens'], is_split_into_words=True, padding=True, truncation=True, max_length=128)
+# Create datasets for each split
+train_data, valid_data, test_data, train_tags, valid_tags, test_tags = split_dataset(dataset)
+tokenized_train_dataset = Dataset.from_dict({'tokens': train_data, 'ner_tags': train_tags}).map(tokenize_function, batched=True)
+tokenized_valid_dataset = Dataset.from_dict({'tokens': valid_data, 'ner_tags': valid_tags}).map(tokenize_function, batched=True)
+tokenized_test_dataset = Dataset.from_dict({'tokens': test_data, 'ner_tags': test_tags}).map(tokenize_function, batched=True)
 
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
-
-# 4. prepare dataset for pytorch
-class NerDataset(Dataset):
-    
-    def __init__(self, tokenized_dataset):
-        self.tokenized_dataset = tokenized_dataset
-
-    def __getitem__(self, idx):
-        input_ids = torch.tensor(self.tokenized_dataset['input_ids'][idx])
-        attention_mask = torch.tensor(self.tokenized_dataset['attention_mask'][idx])
-        labels = torch.tensor(self.tokenized_dataset['ner_tags'][idx], dtype=torch.long)
-        # max_length = 128  # or set it to another value
-
-        # # Apply padding
-        # input_ids = F.pad(input_ids, pad=(0, max_length - len(input_ids)))
-        # attention_mask = F.pad(attention_mask, pad=(0, max_length - len(attention_mask)))
-        # labels = F.pad(labels, pad=(0, max_length - len(labels)))
-
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attention_mask,
-            'labels': labels
-        }
-
-    def __len__(self):
-        return len(self.tokenized_dataset['input_ids'])
-
-train_dataset = NerDataset(tokenized_dataset["train"])
-valid_dataset = NerDataset(tokenized_dataset["validation"])
-test_dataset = NerDataset(tokenized_dataset["test"])
-
-# 5. prepare training arguments and trainer
+# 4. prepare training arguments and trainer
 training_args = TrainingArguments(
-    per_device_train_batch_size=32,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=32,
     output_dir='./results',
-    num_train_epochs=3,
+    num_train_epochs=400,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     logging_dir='./logs',
+    save_total_limit=2,
     logging_steps=100,
     do_train=True,
     do_eval=True,
-    no_cuda=False,
     load_best_model_at_end=True,
+    learning_rate=1e-5,
+    label_names=["labels"],
+    seed = 42
 )
 
 def compute_metrics(p):
@@ -80,7 +95,12 @@ def compute_metrics(p):
         for prediction, label in zip(predictions, labels)
     ]
 
-    results = precision_recall_fscore_support(true_labels, true_predictions)
+    mlb = MultiLabelBinarizer()
+    true_labels_binarized = mlb.fit_transform(true_labels)
+    true_predictions_binarized = mlb.transform(true_predictions)
+
+    results = precision_recall_fscore_support(true_labels_binarized, true_predictions_binarized, average='samples')  # or other averaging method depending on your task
+
     return {
         "precision": results[0],
         "recall": results[1],
@@ -91,18 +111,18 @@ def compute_metrics(p):
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=valid_dataset,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_valid_dataset,
+    tokenizer=tokenizer,
     data_collator=data_collator,
-    compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics
 )
-
 # 6. training
 trainer.train()
 
 # 7. evaluate
-trainer.evaluate()
+trainer.evaluate(tokenized_test_dataset)
 
 # 8. save model
-model.save_pretrained('fine-tuned-roberta-conll2003')
-tokenizer.save_pretrained('fine-tuned-roberta-conll2003')
+model.save_pretrained('fine-tuned-roberta-1output')
+tokenizer.save_pretrained('fine-tuned-roberta-1output')
